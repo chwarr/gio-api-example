8f09995909481c16f16fdb5cc6f62bc7224af56e
Christopher Warrington <c45207@mygcw.net>
2020-04-18 01:36:16 -0700

    Skeleton

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gio_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
